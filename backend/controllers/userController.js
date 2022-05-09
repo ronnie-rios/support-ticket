791c4d15e7dcb4c045f81046c36f7dffbfa9665b
Ronnie Rios <ronniesmail14@gmail.com>
2022-05-09 14:05:23 -0500

    asynchandler added to controller route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,21 +1,23 @@
+const asyncHandler = require('express-async-handler')
+
 //register a new user
 //route /api/users
 //public
-const registerUser = (req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
     const { name, email, password } = req.body
 
     if(!name || !email || !password) {
         res.status(400)
         throw new Error('please include all fields')
     }
-}
+})
 
 //login
 //route /api/users/login
 //public
-const loginUser = (req, res) => {
+const loginUser = asyncHandler(async (req, res) => {
     res.send('login route')
-}
+})
 
 module.exports = {
     registerUser,
