bf3f5aea0861e2607e93251bdffc646fd6c7bdf4
Ronnie Rios <ronniesmail14@gmail.com>
2022-05-11 12:53:49 -0500

    login page created

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,8 +1,68 @@
-import React from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { FaSignInAlt } from 'react-icons/fa'
 
 function Login() {
+  const [formData, setFormData] = useState({
+  
+    email: '',
+    password: '',
+  
+  })
+
+  const {  email, password, pa } = formData
+
+  const onChange = (e) => {
+    setFormData((prev) =>({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault() 
+    
+  }
   return (
-    <div>Login</div>
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt />Login
+        </h1>
+        <p>Please login </p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control"
+              id="name"
+              name={email}
+              value={email}
+              onChange={onChange}
+              placeholder='Please enter your email'
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control"
+              id="name"
+              name={password}
+              value={password}
+              onChange={onChange}
+              placeholder='Please enter your password'
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
   )
 }
 
