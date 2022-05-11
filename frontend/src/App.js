e06501376a20bf2e71f4436ca95dca444f124a50
Ronnie Rios <ronniesmail14@gmail.com>
2022-05-11 12:52:12 -0500

    register form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import Home from './pages/Home';
+import Header from './components/Header';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import './index.css';
 
 function App() {
   return (
-    <div>
-     <h1>hello</h1>
-    </div>
+    <>
+      <Router>
+        <Header />
+        <div className='container'>
+          <Routes>
+            <Route path ='/' element={<Home/>} />
+            <Route path ='/login' element={<Login/>} />
+            <Route path ='/register' element={<Register/>} />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
+    </>
   );
 }
 
---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,95 @@
+import { useState } from 'react'
+import { toast } from 'react-toastify'
+import { FaUser } from 'react-icons/fa'
+
+function Register() {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+
+  const { name, email, password, password2 } = formData
+
+  const onChange = (e) => {
+    setFormData((prev) =>({
+      ...prev,
+      [e.target.name]: e.target.value
+    }))
+  }
+
+  const onSubmit = (e) => {
+    e.preventDefault() 
+    if (password !== password2) {
+      toast.error('passwords do not match')
+    }
+  }
+  return (
+    <>
+      <section className="heading">
+        <h1>
+          <FaUser />
+        </h1>
+        <p>Please create an acoount</p>
+      </section>
+      <section className="form">
+        <form onSubmit={onSubmit}>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control"
+              id="name"
+              name={name}
+              value={name}
+              onChange={onChange}
+              placeholder='Please enter your name'
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control"
+              id="name"
+              name={email}
+              value={email}
+              onChange={onChange}
+              placeholder='Please enter your email'
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control"
+              id="name"
+              name={password}
+              value={password}
+              onChange={onChange}
+              placeholder='Please enter your password'
+              required 
+            />
+          </div>
+          <div className="form-group">
+            <input 
+              type="text" 
+              className="form-control"
+              id="name"
+              name={password2}
+              value={password2}
+              onChange={onChange}
+              placeholder='Please confirm your password'
+              required
+            />
+          </div>
+          <div className="form-group">
+            <button className='btn btn-block'>Submit</button>
+          </div>
+        </form>
+      </section>
+    </>
+  )
+}
+
+export default Register
